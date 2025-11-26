f7a3bd3037bf69068836620ef90bbd0479de5109
Srpboyz <srpboyz69@gmail.com>
2025-11-25 22:01:49 -0800

    Added source filter saving

--- yomu/core/app.py
+++ yomu/core/app.py
@@ -28,7 +28,7 @@ __all__ = ("YomuApp",)
 
 QApplication.setApplicationName("Yomu")
 QApplication.setApplicationDisplayName("Yomu")
-QApplication.setApplicationVersion("1.2.1")
+QApplication.setApplicationVersion("1.2.2")
 if sys.platform == "linux":
     QApplication.setDesktopFileName("yomu")
 
--- yomu/core/sourcemanager.py
+++ yomu/core/sourcemanager.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import importlib
+import json
 import os
 import sys
 from logging import getLogger
@@ -24,7 +25,26 @@ class SourceManager:
     def sources(self) -> tuple[Source]:
         return tuple(self._sources.values())
 
-    def _load_source(self, cls: type[Source]) -> None:
+    def _load_source_filters(self) -> dict:
+        """Get the saved source filters"""
+        path = os.path.join(app_data_path(), "source_filters.json")
+        if not os.path.exists(path):
+            return {}
+
+        try:
+            with open(path) as f:
+                return json.load(f)
+        except Exception:
+            return {}
+
+    def _save_source_filters(self, filters: dict) -> None:
+        """Write the updated source filters to file"""
+        path = os.path.join(app_data_path(), "source_filters.json")
+        with open(path, "w") as f:
+            json.dump(filters, f, indent=4)
+
+    def load_source(self, cls: type[Source]) -> Source | None:
+        """Load source from class"""
         try:
             source = cls(self.app.network)
         except Exception as e:
@@ -35,18 +55,14 @@ class SourceManager:
             return None
 
         if source.id in self._sources:
-            logger.error(
-                f"Duplicate source id between {self._sources[source.id].name} and {source.name}"
-            )
+            logger.error(f"Error adding {source.name} - {source.id} in use")
             return None
 
         self._sources[source.id] = source
+        return source
 
     def _load_sources(self) -> None:
         from _sources import _default_sources
-
-        for cls in _default_sources():
-            self._load_source(cls)
 
         appdata = app_data_path()
         sys.path.append(appdata)
@@ -55,6 +71,7 @@ class SourceManager:
         if not os.path.exists(source_folder):
             os.makedirs(source_folder)
 
+        source_cls = _default_sources()
         for source_dir in os.listdir(source_folder):
             try:
                 mod = importlib.import_module(f".{source_dir}", package="sources")  # fmt:skip
@@ -71,7 +88,16 @@ class SourceManager:
                     and cls is not Source
                     and issubclass(cls, Source)
                 ):
-                    self._load_source(cls)
+                    source_cls.append(cls)
+
+        source_filters = self._load_source_filters()
+        for cls in source_cls:
+            source = self.load_source(cls)
+            if source is None or str(source.id) not in source_filters:
+                continue
+            self.update_source_filters(
+                source, source_filters[str(source.id)], update_file=False
+            )
 
         sys.path.remove(appdata)
 
@@ -79,17 +105,31 @@ class SourceManager:
         return self._sources.get(id)
 
     @overload
-    def update_source_filters(self, source: int, new_filters: dict) -> None: ...
+    def update_source_filters(
+        self, source: int, new_filters: dict, *, update_file: bool = True
+    ) -> None: ...
 
     @overload
-    def update_source_filters(self, source: Source, new_filters: dict) -> None: ...
+    def update_source_filters(
+        self, source: Source, new_filters: dict, *, update_file: bool = True
+    ) -> None: ...
 
-    def update_source_filters(self, source: Source | int, new_filters: dict) -> None:
+    def update_source_filters(
+        self, source: Source | int, new_filters: dict, *, update_file: bool = True
+    ) -> None:
         if isinstance(source, int) and (source := self.get_source(source)) is None:
             return
 
         try:
-            if bool(source.update_filters(new_filters)):
-                self.app.source_filters_updated.emit(source, new_filters)
-        except Exception:
-            pass
+            if not bool(source.update_filters(new_filters)):
+                return
+        except Exception as e:
+            logger.error(f"Failed to update filters for {source.name}", exc_info=e)
+            return
+
+        if update_file:
+            source_filters = self._load_source_filters()
+            source_filters[str(source.id)] = new_filters
+            self._save_source_filters(source_filters)
+
+        self.app.source_filters_updated.emit(source, new_filters)
