e00c7ea481ad65788c34d291c825be0264900e97
Srpboyz <srpboyz69@gmail.com>
2026-01-15 14:56:11 -0800

    Added animations for single page reader

--- yomu/ui/reader/view/singlepage.py
+++ yomu/ui/reader/view/singlepage.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
 from enum import IntEnum
-from typing import Callable, override, TYPE_CHECKING
+from typing import Callable, TYPE_CHECKING
 
-from PyQt6.QtCore import QEvent, QPoint, QPoint, QRect, QSize
+from PyQt6.QtCore import (
+    QEasingCurve,
+    QParallelAnimationGroup,
+    QPoint,
+    QPropertyAnimation,
+    QRect,
+    QSize,
+)
 from PyQt6.QtWidgets import QMenu, QHBoxLayout, QStackedLayout, QWidget
 
 from yomu.core import utils as core_utils
@@ -13,6 +20,12 @@ from .base import BaseView
 
 if TYPE_CHECKING:
     from yomu.ui.reader import Reader
+
+
+class AnimationGroup(QParallelAnimationGroup):
+    def stop(self) -> None:
+        self.finished.emit()
+        return super().stop()
 
 
 class PageWidget(QWidget):
@@ -42,6 +55,7 @@ class StackLayout(QStackedLayout):
         super().__init__(view)
         self.reader = view.reader
         self.direction = StackLayout.FitDirection.Height
+        self._animation = None
 
         self.setContentsMargins(0, 0, 0, 0)
         self.setSpacing(0)
@@ -53,37 +67,124 @@ class StackLayout(QStackedLayout):
             return item.widget()
         return None
 
-    def fit_to_width(self, image_size: QSize) -> tuple[int, int]:
+    def fit_to_width(self, image_size: QSize) -> QRect:
         reader_size = self.reader.size()
 
         width = reader_size.width()
         height = round(width * image_size.height() / image_size.width())
 
-        return width, height
+        if reader_size.height() > height:
+            y = round((reader_size.height() - height) / 2)
+            return QRect(0, y, width, height)
+        return QRect(0, 0, width, height)
 
-    def fit_to_height(self, image_size: QSize) -> tuple[int, int]:
+    def fit_to_height(self, image_size: QSize) -> QRect:
         reader_size = self.reader.size()
 
         width = round(reader_size.height() * (image_size.width() / image_size.height()))
         height = reader_size.height()
 
-        return width, height
+        if reader_size.width() > width:
+            x = round((reader_size.width() - width) / 2)
+            return QRect(x, 0, width, height)
+        return QRect(0, 0, width, height)
 
-    @override
+    def calculate_target_geometry(self, widget: PageWidget) -> QRect:
+        if widget.page_status == PageView.Status.LOADED:
+            image_size = widget.image_size()
+            if self.direction == StackLayout.FitDirection.Width:
+                return self.fit_to_width(image_size)
+            else:
+                return self.fit_to_height(image_size)
+        return QRect(QPoint(0, 0), self.reader.size())
+
+    def _slide_to_widget(self, index: int) -> None:
+        if self._animation:
+            self._animation.stop()
+
+        current_index = self.currentIndex()
+        if current_index == index:
+            return QStackedLayout.setCurrentIndex(self, index)
+
+        right_to_left = index > current_index
+
+        current_page = self.currentWidget()
+        new_page = self.page_widget_at(index)
+
+        reader_width = self.reader.width()
+
+        current_page_animation = QPropertyAnimation(current_page, b"geometry")
+        current_page_animation.setDuration(300)
+        current_page_animation.setEasingCurve(QEasingCurve.Type.Linear)
+        current_page_animation.setStartValue(current_page.geometry())
+        current_page_animation.setEndValue(
+            QRect(
+                -current_page.width() if right_to_left else reader_width,
+                current_page.y(),
+                current_page.width(),
+                current_page.height(),
+            )
+        )
+
+        new_page_geometry = self.calculate_target_geometry(new_page)
+        new_page_animation = QPropertyAnimation(new_page, b"geometry")
+        new_page_animation.setDuration(300)
+        new_page_animation.setEasingCurve(QEasingCurve.Type.Linear)
+        new_page_animation.setStartValue(
+            QRect(
+                reader_width if right_to_left else -new_page_geometry.width(),
+                new_page_geometry.y(),
+                new_page_geometry.width(),
+                new_page_geometry.height(),
+            )
+        )
+        new_page_animation.setEndValue(new_page_geometry)
+
+        self._animation = AnimationGroup(self)
+        self._animation.addAnimation(current_page_animation)
+        self._animation.addAnimation(new_page_animation)
+
+        def on_animation_finished():
+            QStackedLayout.setCurrentIndex(self, index)
+            self._animation = None
+
+        new_page.show()
+        self._animation.finished.connect(on_animation_finished)
+        self._animation.start(QParallelAnimationGroup.DeletionPolicy.DeleteWhenStopped)
+
+    def setCurrentIndex(self, index: int) -> None:
+        self._slide_to_widget(index)
+
+    def setCurrentWidget(self, w: QWidget | None) -> None:
+        index = self.indexOf(w)
+        if index != -1:
+            self._slide_to_widget(index)
+
     def setGeometry(self, rect: QRect) -> None:
         current_widget = self.currentWidget()
+        view: SinglePageView = self.parentWidget()
         if current_widget is not None:
             if current_widget.page_status == PageView.Status.LOADED:
                 image_size = current_widget.image_size()
-                width, height = (
-                    self.fit_to_width(image_size)
-                    if self.direction == StackLayout.FitDirection.Width
-                    else self.fit_to_height(image_size)
-                )
-                rect = QRect(rect.x(), rect.y(), width, height)
+                if self.direction == StackLayout.FitDirection.Width:
+                    rect = self.fit_to_width(image_size)
+                    view.setFixedSize(
+                        self.reader.size()
+                        if self.reader.height() > rect.height()
+                        else rect.size()
+                    )
+                else:
+                    rect = self.fit_to_height(image_size)
+                    view.setFixedSize(
+                        self.reader.size()
+                        if self.reader.width() > rect.width()
+                        else rect.size()
+                    )
             else:
                 rect = QRect(QPoint(0, 0), self.reader.size())
-
+                view.setFixedSize(self.reader.size())
+        else:
+            view.setFixedSize(self.reader.size())
         super().setGeometry(rect)
 
 
@@ -105,48 +206,24 @@ class SinglePageView(BaseView):
 
     layout: Callable[[], StackLayout]
 
-    def eventFilter(self, a0: QWidget, a1: QEvent) -> bool:
-        if (
-            a0 == self.layout().page_widget_at(self.page)
-            and a1.type() == QEvent.Type.Resize
-        ):
-            self.setFixedSize(a1.size())
-        return super().eventFilter(a0, a1)
-
     def _reader_resized(self, _) -> None:
         self.layout().update()
 
     def _current_changed(self, page: int):
         self.current_index = page
-        if self.page_count < 1:
-            return
-
-        page_widget = self.layout().page_widget_at(self.page)
-        if page_widget is not None:
-            self.setFixedSize(page_widget.size())
-
-        if page == self.page_count - 1:
+        if self.page_count > 0 and page == self.page_count - 1:
             self.reader.mark_chapter_as_read()
 
     def _page_changed(self, page: int) -> None:
-        if page < 0:
-            return
-
-        layout = self.layout()
-        layout.setCurrentIndex(page)
-        self.reader.verticalScrollBar().setValue(0)
-
-        page_widget = layout.page_widget_at(self.page)
-        if page_widget is not None:
-            self.setFixedSize(page_widget.size())
+        if page > -1:
+            self.layout().setCurrentIndex(page)
+            self.reader.verticalScrollBar().setValue(0)
 
     def set_page_views(self, views: list[PageView]) -> None:
         layout = self.layout()
         layout.blockSignals(True)
         for view in views:
-            page_widget = PageWidget(self, view)
-            page_widget.installEventFilter(self)
-            layout.addWidget(page_widget)
+            layout.addWidget(PageWidget(self, view))
         layout.blockSignals(False)
 
     def take_page_views(self) -> list[PageView]:
@@ -178,7 +255,7 @@ class SinglePageView(BaseView):
 
     def page_at(self, pos: QPoint) -> PageView | None:
         page_widget = self.layout().currentWidget()
-        if page_widget.geometry().contains(self.mapFromParent(pos)):
+        if page_widget and page_widget.geometry().contains(self.mapFromParent(pos)):
             return page_widget.page_view
 
     def zoom_out(self) -> None: ...
