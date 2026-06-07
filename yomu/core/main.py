4cb2b65fdcfbe78a6b891c081a9cb7c6fea3004e
Srpboyz <srpboyz69@gmail.com>
2026-06-06 22:13:34 -0700

    Updated cmd line

--- yomu/core/main.py
+++ yomu/core/main.py
@@ -1,43 +1,47 @@
-from PyQt6.QtCore import QCommandLineOption, QCommandLineParser, QObject
-
+from argparse import ArgumentParser, Namespace
 from .app import YomuApp
 from .ipc import IPCSocket
 
 
-def _run_cli(socket: IPCSocket, argv: list[str]) -> int:
-    parser = QCommandLineParser()
-    parser.addHelpOption()
-    parser.addVersionOption()
-
-    showWindow = QCommandLineOption(["show"], socket.tr("Show the window"))
-    newWindow = QCommandLineOption(["n", "new-window"], socket.tr("Opens a new window"))
-    openSource = QCommandLineOption(
-        ["s", "source"], QObject.tr("Open a source"), "source"
+def parse_args(argv: list[str]) -> Namespace:
+    parser = ArgumentParser("yomu")
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"%(prog)s {YomuApp.applicationVersion()}",
     )
+    parser.add_argument("--show", action="store_true", help="Show the window")
+    parser.add_argument(
+        "-n", "--new-window", action="store_true", help="Opens a new window"
+    )
+    parser.add_argument(
+        "-s",
+        "--source",
+        help="Open a source. This won't work unless app is already running",
+    )
+    return parser.parse_args(argv)
 
-    parser.addOption(showWindow)
-    parser.addOption(newWindow)
-    parser.addOption(openSource)
-    parser.process(argv)
 
-    showWindow = parser.isSet(showWindow) or not parser.isSet(newWindow)
-    if showWindow:
+def handle_args(socket: IPCSocket, args: Namespace) -> int:
+    show_window = args.show or not args.new_window
+    if show_window:
         data = socket.show_window()
         if not data["success"]:
             YomuApp.display_message(
                 f"Failed to show window. {data.get('reason', 'No reason provided')}"
             )
             return 1
-        winId = data["winId"]
+        win_id = data["winId"]
     else:
         data = socket.open_window()
         if not data["success"]:
             YomuApp.display_message(f"Failed to open a new window. {data['reason']}")
             return 1
-        winId = data["winId"]
+        win_id = data["winId"]
 
-    if parser.isSet(openSource):
-        data = socket.open_source(parser.value(openSource), winId)
+    if args.source:
+        data = socket.open_source(args.source, win_id)
         if not data["success"]:
             YomuApp.display_message(f"Failed to open a new window. {data['reason']}")
             return 1
@@ -47,7 +51,8 @@ def _run_cli(socket: IPCSocket, argv: list[str]) -> int:
 
 
 def main(argv: list[str]) -> int:
+    args = parse_args(argv[1:])
     socket = IPCSocket()
     if socket.connectToServer():
-        return _run_cli(socket=socket, argv=argv)
+        return handle_args(socket=socket, args=args)
     return YomuApp(argv=argv).exec()
